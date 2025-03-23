b67f7b9d242af24c547f72096111d9158a233de0
Mansi090 <mansid875@gmail.com>
2025-03-23 19:54:37 +0530

    initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+        Passionate software developer with expertise in C++, Python, ML and web development. I enjoy solving complex problems through DSA, open-source contributions, and AI-driven projects. Currently exploring machine learning and building innovative web applications.Open Source contributor, and tech enthusiast. Always eager to learn and collaborate! 
+
+
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
